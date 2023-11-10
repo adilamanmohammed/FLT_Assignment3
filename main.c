398c8af96c2ce3de74d3bef6395d8f4ff0d78665
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-11-10 16:07:31 -0600

    Update main.c

--- main.c
+++ main.c
@@ -10,142 +10,10 @@ Description: The following code is logic for implementing the Minimization of DF
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <stdbool.h>
 #define MAX 1000
-char buffer[MAX][MAX][MAX],string[MAX],statenum,sbuff[MAX][MAX],newclasses[MAX][MAX],KA[1000],A[1000],noreach[1000],appendednum[MAX],notreachable[MAX];
-int buffindex=0,DFSM=1,position=-1,found,sindex=0,scount=0,subcount=0,alphalength,marked[MAX] = {0},    fileIndex = 0,charIndex;
-
-/*
-//function to verify the alphabet is present and return the position for use of logic
-int Verify_and_store_alphabet_position(const char *buffer, char target)
-{
-	//int position=-1;
-
-	for(int i=0;i<strlen(buffer);i++)
-	{
-		if(buffer[i]==target)
-		{
-			position=i;
-			break; //Searching Stopped once the position is found
-		}
-	}
-	return position;
-
-}
-
-//used to flag the states which are reachable
-void mark_state_which_are_reachable(int currentState, int* marked, int numStates,int symbol) {
-    marked[currentState] = 1;
-
-    for (int i = 0; i < symbol; i++) {
-        int nextState = buffer[currentState][i] - '0'; 
-        if (nextState >= 1 && nextState <= numStates && !marked[nextState]) {
-            mark_state_which_are_reachable(nextState, marked, numStates,symbol);
-        }
-    }
-}
-
-//check if the string belongs to which equivalence_state
-int belongs_to_which_equivalence_state(const char *string)
-{
-    int DFSM=1;
-
-    int length=strlen(string);
-    for(int i=0;i<length;i++)
-    {
-        int position =Verify_and_store_alphabet_position(buffer[0],string[i]);
-        statenum=buffer[DFSM][position];
-        //coverting character to integer and intialize to DFSM
-        DFSM=statenum-'0';
-
-    }
-    return DFSM;
-
-}
-
-void savegroup(const char *string, char groupnum)
-{
-    char filename[1000];  // Adjust the size according to your needs
-    sprintf(filename, "group_%d.txt", groupnum);
-
-    FILE *file = fopen(filename, "a");
-    if (file == NULL) {
-        printf("Error opening file: %s\n", filename);
-        return;
-    }
-
-    fprintf(file, "%s\n", string);
-    fclose(file);
-}
-
-void append_character(char newclasses[], char ch) {
-    // Find the null-terminator in the newclasses array
-    int len = strlen(newclasses);
-
-    // Append the character to the end of the array
-    newclasses[len] = ch;
-    
-    // Null-terminate the new string
-    newclasses[len + 1] = '\0';
-}
-
-
-// Checking if x is present in buffer[buffindex-1]
-void appendToAorKA(int x) {
-
-    // Convert integer x to character
-    char x_char = x + '0'; 
-    int found = 0;
-
-    for (int i = 0; buffer[buffindex - 1][i] != '\0'; i++) {
-        if (buffer[buffindex - 1][i] == x_char) {
-            found = 1;
-            break;
-        }
-    }
-
-    // Append x_char to KA or A based on the condition
-    if (found) {
-        strcat(A, &x_char);
-    } else {
-        strcat(KA, &x_char);
-    }
-
-    
-}
-
-
-
-int find_character_present_in_array(const char *array, char target) {
-    while (*array != '\0') {
-        if (*array == target) {
-            // Character found in the array
-            return 1; 
-        }
-        array++;
-    }
-    
-    return 0; // Character not found in the array
-}
-
-
-
-int checkgroup(int character) {
-    int foundRow = -1; // Initialize to -1 to indicate not found
-    char A=character+'0';
-
-    for(int i=0;i<scount;i++)
-    {
-        if(find_character_present_in_array(newclasses[i],A)==1)
-        {
-            return i;
-            break;
-        }
-    }
-    
-}
-
-
-*/
+char buffer[MAX][MAX][MAX],string[MAX],statenum,sbuff[MAX][MAX],newclasses[MAX][MAX],notreachable[MAX];
+int buffindex=0,DFSM=1,position=-1,found,sindex=0,scount=0,subcount=0,appended_count=0,alphalength,marked[MAX] = {0},appendednum[MAX],size=0,    fileIndex = 0,charIndex,noreach[1000],KA[1000],A[1000],A_count = 0,KA_count = 0;
 
 
 int stringcount(int A)
@@ -168,6 +36,115 @@ void printcontent(char buffer[][MAX][MAX],int buffindex,int A)
         printf("\n");
     }
 }
+
+
+bool visited[MAX] = {false}; // Keep track of visited states
+
+void dfs(int state, char buffer[][MAX][MAX], int numStates, int alphalength) {
+    visited[state] = true;
+    for (int i = 0; i < alphalength; i++) {
+        int nextState = atoi(buffer[state + 1][i]); // Already 1-based indexing from buffer
+        if (nextState > 0 && nextState <= numStates && !visited[nextState - 1]) { // Convert to 0-based index for visited
+            dfs(nextState - 1, buffer, numStates, alphalength);
+        }
+    }
+}
+
+
+int findNonReachableStates(char buffer[][MAX][MAX], int numStates, int alphalength, int noreach[]) {
+    // Fill the visited array by running dfs starting from state 1 (which is the first state in buffer[1])
+    dfs(0, buffer, numStates, alphalength);
+
+    int count = 0;
+    for (int i = 0; i < numStates; i++) {
+        if (!visited[i]) {
+            noreach[count++] = i + 1; // Store non-reachable states as 1-based index
+        }
+    }
+    return count; // Return the count of non-reachable states
+}
+
+void printNonReachableStates(int noreach[], int count) {
+    printf("Non-reachable States: ");
+    if (count == 0) {
+        printf("None\n");
+    } else {
+        for (int i = 0; i < count; i++) {
+            printf("%d ", noreach[i]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+bool isInNoReach(int state, int noreach[], int count) {
+    for (int i = 0; i < count; i++) {
+        if (noreach[i] == state) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isFinalState(int state, int A[], int A_count) {
+    for (int i = 0; i < A_count; i++) {
+        if (A[i] == state) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int findNonFinalReachableStates(int numStates, int noreach[], int nonReachableCount, int A[], int A_count, int KA[]) {
+    int KA_count = 0;
+    for (int state = 1; state <= numStates; state++) {
+        if (!isInNoReach(state, noreach, nonReachableCount) && !isFinalState(state, A, A_count)) {
+            KA[KA_count++] = state;
+        }
+    }
+    return KA_count;
+}
+
+
+
+// Function to find if an integer is present in an array
+int find_integer_present_in_array(const int A[], int size, int target) {
+    for (int j = 0; j < size; j++) {
+        if (A[j] == target) {
+            return 1; // Integer found in the array
+        }
+    }
+    return 0; // Integer not found in the array
+}
+
+void append_character(char newclasses[], char ch) {
+    // Find the null-terminator in the newclasses array
+    int len = strlen(newclasses);
+
+    // Append the character to the end of the array
+    newclasses[len] = ch;
+    
+    // Null-terminate the new string
+    newclasses[len + 1] = '\0';
+}
+
+
+
+
+
+void append_integer(int array[], int *size, int value) {
+    // Add the integer to the end of the array
+    array[*size] = value;
+    (*size)++; // Increase the size of the array
+}
+
+
+
+
+
+
+
 
 //main function
 int main(int argc, char *argv[])
@@ -196,26 +173,23 @@ int main(int argc, char *argv[])
     
     char line[MAX * MAX]; // Buffer to hold each line of text, assuming each token can be up to MAX characters
 
-    // Read lines and store each token as a string in the buffer
+    // Read the file line by line
     while (fgets(line, sizeof(line), file1)) {
-        // Check if the line is not just a newline or space
-        if (strspn(line, " \t\r\n") != strlen(line)) {
-            char *token = strtok(line, " \t\n");
-            int tokenIndex = 0;
-            while (token != NULL && tokenIndex < MAX) {
-                strncpy(buffer[buffindex][tokenIndex], token, MAX);
-                buffer[buffindex][tokenIndex][MAX - 1] = '\0'; // Ensure null-termination
-                tokenIndex++;
-                token = strtok(NULL, " \t\n");
+        // Ignore empty lines
+        if (strcmp(line, "\n") != 0) {
+            int col = 0;
+            // Split the line by spaces and store each part in the buffer
+            char *token = strtok(line, " ");
+            while (token) {
+                if (strlen(token) > 0) {
+                    // Remove new line characters if present
+                    token[strcspn(token, "\n")] = 0;
+                    strcpy(buffer[buffindex][col], token);
+                    col++;
+                }
+                token = strtok(NULL, " ");
             }
-            if (tokenIndex > 0) { // If at least one token was found
-                buffindex++;
-            }
-        }
-
-        if (buffindex >= MAX) {
-            // Handle the case where the maximum number of lines has been reached
-            break;
+            buffindex++;
         }
     }
 
@@ -353,7 +327,7 @@ int main(int argc, char *argv[])
     }
 
 
-
+    
 
 
     //check if given final states are valid for given DFSM
@@ -367,67 +341,64 @@ int main(int argc, char *argv[])
 
     }
 
-
-/*
-    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<Actual logic >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    //removing not reachable states from state 2 to last state
+    int nonReachableCount = findNonReachableStates(buffer, buffindex - 2, alphalength, noreach); // Use buffindex - 2 because last line is final states
+    printNonReachableStates(noreach, nonReachableCount);
 
 
-    //Doing DFS from state 1 and removing not reachable states from state 2 to last state
-    mark_state_which_are_reachable(1, marked, buffindex-2,alphalength);  // Since you mentioned transition[1] is initial state and your indexing is 1-based.
-
-    // Printing the unreachable states
-    for (int i = 1; i <= buffindex-2; i++) 
-    {   // Using 1-based indexing loop
-        if (!marked[i]) {
-            append_character(noreach,i+'0');
-        }
+    A_count = 0; // Initialize the counter for number of final states
+    for (int i = 0; buffer[buffindex - 1][i][0] != '\0'; i++) {
+        A[A_count++] = atoi(buffer[buffindex - 1][i]); // Convert string to int and store in A
     }
 
-    //debug norech
-    //printf("Unreachable states: ");
-    for(int i=0;i<strlen(noreach);i++)
-    {
-        printf(" %c",noreach[i]);
+    // Print the final states stored in A
+    printf("A: ");
+    for (int i = 0; i < A_count; i++) {
+        printf("%d ", A[i]);
     }
     printf("\n");
 
-    
 
-    //append to A or KA
-    for(int i=1;i<buffindex-1;i++)
+    /*for(int i=0;i<buffindex;i++)
     {
-        char char_i = i + '0';
-
-        if(strchr(noreach,i+'0'))
+        for(int j=0;j<alphalength;j++)
         {
-
+           printf("\nbuffer[%d][%d]:%s",i,j,buffer[i][j]); 
         }
-        else
-        appendToAorKA(i);
-    }
-    //Print A and KA
-    //printf("A: %s\n", A);
-    //printf("KA: %s\n", KA);
-    //printf("no reach: %s\n\n", noreach);
+    }*/
+    
+    //printf("\n\n%s\n",buffer[12][0]);
 
-    //printf("for non final:\n");
+    // Find non-final, reachable states
+    KA_count = findNonFinalReachableStates(buffindex - 2, noreach, nonReachableCount, A, A_count, KA);
+
+    // Print the non-final, reachable states
+    printf("KA: ");
+    for (int i = 0; i < KA_count; i++) {
+        printf("%d ", KA[i]);
+    }
+    printf("\n");
+
+
+    printf("For non final :\n");
     //check and make eqivalence states for non final logic
     for(int i=1;i<buffindex-1;i++)
     {   subcount=0;
-        int infinal=find_character_present_in_array(A,i+'0');
-        int flag=find_character_present_in_array(appendednum,i+'0');
-        int noreachflag=find_character_present_in_array(noreach,i+'0');
+        printf("entered logic A@%d\n",i);
+        int infinal=find_integer_present_in_array(A,A_count,i);
+        int flag = find_integer_present_in_array(appendednum, size, i );
+        printf("flag=%d\n",flag);
+        int noreachflag=find_integer_present_in_array(noreach,nonReachableCount,i);
         if(flag==1 || infinal==1 || noreachflag==1)
         {
-            //printf("skip@%d\n",i);
+            printf("skip@%d\n",i);
         }
         else if(i==buffindex-2)
         {
             newclasses[scount][subcount]=i+'0';
-            append_character(appendednum,i+'0');
-            //printf("newclass[%d][%d]=%c\n",scount,subcount,i+'0');
+            append_integer(appendednum,&size,i);
+            appended_count++;
+            printf("appended %d@i=%d\n",i,i);
+            printf("newclass[%d][%d]=%c\n",scount,subcount,i+'0');
             scount++;
         }
         else
@@ -437,7 +408,9 @@ int main(int argc, char *argv[])
             if(len==0)
             {
                 newclasses[scount][subcount]=i+'0';
-                append_character(appendednum,i+'0');
+                append_integer(appendednum,&size,i);
+                appended_count++;
+                printf("appended %d@i=%d\n",i,i);
                 tflag++;
                 subcount++;
             }
@@ -445,14 +418,16 @@ int main(int argc, char *argv[])
             for(int j=i+1;j<buffindex-1;j++)
             {
                 
-                int finalflag=find_character_present_in_array(A,j+'0');
+                int finalflag=find_integer_present_in_array(A,A_count,j);
                 if(finalflag!=1)
                 {
-                    int compare=strcmp(buffer[i],buffer[j]);
+                    int compare=strcmp(buffer[i][0],buffer[j][0]);
                     if(compare==0)
                     {
                         newclasses[scount][subcount]=j+'0';
-                        append_character(appendednum,j+'0');
+                        append_integer(appendednum,&size,j);
+                        appended_count++;
+                        printf("appended %d@i=%d\n",j,i);
                         tflag++;
                         subcount++;
                     }
@@ -470,25 +445,30 @@ int main(int argc, char *argv[])
         
     }
 
+printf("\n\n");
 
 
-    //printf("for final states:\n");
-    //check and make eqivalence states for non final logic
+printf("For final :\n");
+//check and make eqivalence states for final logic
     for(int i=1;i<buffindex-1;i++)
     {   subcount=0;
-        int in_non_final=find_character_present_in_array(KA,i+'0');
-        int flag=find_character_present_in_array(appendednum,i+'0');
-        int noreachflag=find_character_present_in_array(noreach,i+'0');
+        printf("entered logic A@%d\n",i);
+        int in_non_final=find_integer_present_in_array(KA,KA_count,i);
+        int flag = find_integer_present_in_array(appendednum, size, i );
+        printf("flag=%d\n",flag);
+        int noreachflag=find_integer_present_in_array(noreach,nonReachableCount,i);
         if(flag==1 || in_non_final==1 || noreachflag==1)
         {
-            //skip if not in appended or in in final or if in no reach
-            //printf("skip@%d\n",i);
+            printf("skip@%d\n",i);
         }
         else if(i==buffindex-2)
         {
             newclasses[scount][subcount]=i+'0';
-            append_character(appendednum,i+'0');
-            //printf("newclass[%d][%d]=%c\n",scount,subcount,i+'0');
+            append_integer(appendednum,&size,i);
+            appended_count++;
+            printf("appended %d@i=%d\n",i,i);
+            printf("newclass[%d][%d]=%c\n",scount,subcount,i+'0');
+            scount++;
         }
         else
         {
@@ -497,21 +477,26 @@ int main(int argc, char *argv[])
             if(len==0)
             {
                 newclasses[scount][subcount]=i+'0';
-                append_character(appendednum,i+'0');
+                append_integer(appendednum,&size,i);
+                appended_count++;
+                printf("appended %d@i=%d\n",i,i);
                 tflag++;
                 subcount++;
             }
             
             for(int j=i+1;j<buffindex-1;j++)
             {
-                int nonfinalflag=find_character_present_in_array(KA,j+'0');
-                if(nonfinalflag!=1)
+                
+                int finalflag=find_integer_present_in_array(KA,KA_count,j);;
+                if(finalflag!=1)
                 {
-                    int compare=strcmp(buffer[i],buffer[j]);
+                    int compare=strcmp(buffer[i][0],buffer[j][0]);
                     if(compare==0)
                     {
                         newclasses[scount][subcount]=j+'0';
-                        append_character(appendednum,j+'0');
+                        append_integer(appendednum,&size,j);
+                        appended_count++;
+                        printf("appended %d@i=%d\n",j,i);
                         tflag++;
                         subcount++;
                     }
@@ -530,35 +515,53 @@ int main(int argc, char *argv[])
     }
 
 
-    //printf("\n");
-   //check debug loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+printf("\n");
+    for (int i = 0; i < size; i++) {
+       printf("appendednum[%d]: %d\n", i, appendednum[i]);
+    }
+    
+
+
+
+
+//check debug loop
     printf("These are the new equivalence states:\n");
     for (int i = 0; i < scount; i++) {
        printf("newclasses[%d]: %s\n", i+1, newclasses[i]);
     }
     printf("Note: With respect to above state-numbers group numbers are given\n");
-    //debug
-    //printf("append: %s\n",appendednum);
-    //printf("scount: %d\n\n",scount);
 
 
-    //check strings.txt and check each string belongs to which group
-    for(int i=0;i<sindex;i++)
-    {
-        int class_state=belongs_to_which_equivalence_state(sbuff[i]);
-        int check=checkgroup(class_state);
-        //printf("%s belongs : %d , and final is %d\n",sbuff[i], class_state,check);
-        savegroup(sbuff[i],check+1);
+
+
+    
+
+
+
+
+    
+    }
+
 
     }
 
-    }
 
-    //end of first else 
-	} 
-*/
-	return 0;
-
-    }
-}
+return 0;
 }
